bd7fda4ef543c7a184e45e7241c32c307f430620
Jordi Baulenas Martín <jordibaulenas@ceroca.cat>
2025-10-02 15:58:12 +0200

    Moviment nau y limits pantalla

--- Assets/Scripts/NouJugador1.cs
+++ Assets/Scripts/NouJugador1.cs
@@ -1,9 +1,11 @@
+using System;
 using UnityEngine;
 
 public class NewMonoBehaviourScript : MonoBehaviour
 {
     public float vel = 10f;
-    
+    private Vector3 limitInferiorEsquerra;
+    private Vector3 limitSuperiorDret;
     private Camera camera;
 
     //public GameObject jugador;
@@ -11,54 +13,76 @@ public class NewMonoBehaviourScript : MonoBehaviour
     void Start()
     {
         camera = Camera.main;
+
+        float distanciaZCameraNau = Mathf.Abs(transform.position.z - camera.transform.position.z);
+
+        limitInferiorEsquerra = camera.ViewportToWorldPoint(new Vector3(0, 0, distanciaZCameraNau));
+        limitSuperiorDret = camera.ViewportToWorldPoint(new Vector3(1, 1, distanciaZCameraNau));
     }
 
     // Update is called once per frame
     void Update()
     {
 
-    MovimentNau();
+        MovimentNau();
+        ControlLimitsPantalla();
     }
 
-    void MovimentNau(){
+    void MovimentNau()
+    {
 
         /*if (Input.GetKeyDown(KeyCode.RightArrow) || Input.GetKeyDown(KeyCode.D))
             
         */
 
         //Control limits pantalla
-        Vector3 limitInferiorEsquerra = camera.ViewportToWorldPoint(new Vector2(0, 0));
-        Vector3 limitSuperiorDret = camera.ViewportToWorldPoint(new Vector2(1, 1));
+
 
         // Moviment nau.
-        
+
         float direccioHoritzontal = Input.GetAxisRaw("Horizontal");
-        float direccioVertical = Input.GetAxis("Vertical");
+        float direccioVertical = Input.GetAxisRaw("Vertical");
         Vector3 direccio = new Vector3(direccioHoritzontal, direccioVertical, 0).normalized; //(x, y, z)
-        //Debug.Log("direccio =" + direccio);
+        Debug.Log("direccio =" + direccio);
         //transform.position = direccio;
 
         Vector3 nouDesplacament = new Vector3(
         vel * direccio.x * Time.deltaTime,
-        vel *  direccio.y * Time.deltaTime,
+        vel * direccio.y * Time.deltaTime,
         vel * direccio.z * Time.deltaTime
         );
         //Debug.Log("Time.deltaTime =" + Time.deltaTime);
-        
+
         //Control dels limits de la pantalla
-        
+
         //El metode Math.clamp, ens retorna el primer parametre.
-        
+
         /*
         Per, si aquet primer parametre, te un valor mes petit que el segon parametre, 
         retornara el segon parametre. I, si te un valor mes gran que el tercer parametre, 
         ens retornara el tercer parametre.
         */
-        nouDesplacament.x = Math.Clamp(nouDesplacament.x, limitInferiorEsquerra.x, limitSuperiorDret.x);
-        nouDesplacament.x = Math.Clamp(nouDesplacament.y, limitInferiorEsquerra.y, limitSuperiorDret.y);
-
         // Apliquem el vector desplaçament a l'objecte
-        transform.position += nouDesplacament; 
+        transform.position += nouDesplacament;
+    }
+    void ControlLimitsPantalla()
+    {
+        /*
+        No podem modificar el transfomr.position.x, nomes consultar-ho, per tant,
+         fem una nova variable 
+         */
+        Vector3 novaPos = transform.position;
+        //Control dels limits de la pantalla
+        //El metode Math.clamp, ens retorna el primer parametre.
+        /*
+        Per, si aquet primer parametre, te un valor mes petit que el segon parametre, 
+        retornara el segon parametre. I, si te un valor mes gran que el tercer parametre, 
+        ens retornara el tercer parametre.
+        */
+        novaPos.x = Math.Clamp(novaPos.x, limitInferiorEsquerra.x, limitSuperiorDret.x);
+        novaPos.y = Math.Clamp(novaPos.y, limitInferiorEsquerra.y, limitSuperiorDret.y);
+
+        transform.position = novaPos;
     }
     
 }
